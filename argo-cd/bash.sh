056c84b913213d88947b564f9dce31bd9a4aa54c
marcosi23 <i.marcos4258@gmail.com>
2023-11-06 23:31:27 -0300

    updaet

---
+++ argo-cd/bash.sh
@@ -0,0 +1,8 @@
+YOUR_ARGOCD_NAMESPACE="argo-cd" # e.g. argo-cd
+YOUR_ARGOCD_RELEASENAME="argocd" # e.g. argo-cd
+
+for crd in "applications.argoproj.io" "applicationsets.argoproj.io" "argocdextensions.argoproj.io" "appprojects.argoproj.io"; do
+  kubectl label --overwrite crd $crd app.kubernetes.io/managed-by=Helm
+  kubectl annotate --overwrite crd $crd meta.helm.sh/release-namespace="$YOUR_ARGOCD_NAMESPACE"
+  kubectl annotate --overwrite crd $crd meta.helm.sh/release-name="$YOUR_ARGOCD_RELEASENAME"
+done
